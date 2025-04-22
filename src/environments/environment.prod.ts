a32ea39e731d59162e28f5b49b84a7725237d514
Aleksandr <amesemyta@gmail.com>
2025-04-22 18:54:51 +0300

    fix: update environment variables handling with .env approach

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,7 @@
-declare global {
-  interface Window {
-    _env_: {
-      API_KEY: string;
-    }
-  }
-}
+declare const ng: any;
 
 export const environment = {
   production: true,
   apiUrl: 'https://api.example.com', // Замените на ваш реальный API URL
-  apiKey: window._env_?.API_KEY || ''
+  apiKey: (typeof ng !== 'undefined' ? ng.process.env.NG_APP_API_KEY : '') || ''
 }; 
